3db9fd89d0d026569cff8dd87c21590175010b56
François Houlbrèque <francois.houlbreque@gmail.com>
2019-03-23 21:34:56 +0100

    Implement show details functionality for the question.

--- app/Question.php
+++ app/Question.php
@@ -53,7 +53,7 @@ class Question extends Model
 
     public function getUrlAttribute()
     {
-        return route("questions.show", $this->id);
+        return route("questions.show", $this->slug);
     }
 
     public function getCreatedDateAttribute()
@@ -71,5 +71,10 @@ class Question extends Model
         }
 
         return "unanswered";
+    }
+
+    public function getBodyHtmlAttribute()
+    {
+        return \Parsedown::instance()->text($this->body);
     }
 }
