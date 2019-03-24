f12ed73aae26b29456549a4f2fd4a3262bbc8c93
François Houlbrèque <francois.houlbreque@gmail.com>
2019-03-24 22:55:42 +0100

    Implement answers display in the Questions details.

--- app/User.php
+++ app/User.php
@@ -82,6 +82,13 @@ class User extends Authenticatable
         return '#';
     }
 
+    public function getAvatarAttribute()
+    {
+        $email = $this->email;
+        $size = 36;
+
+        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?s=" . $size;
+    }
 
 
 
