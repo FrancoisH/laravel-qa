173bac1954b69ed2b82b8e67589015193a734b81
François Houlbrèque <francois.houlbreque@gmail.com>
2019-03-24 15:29:58 +0100

    Implement authorization using Policies

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class QuestionsController extends Controller
 {
+
+    public function __construct() {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -61,23 +66,30 @@ class QuestionsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Question  $question
+     * @param  \App\Question $question
+     *
      * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
     public function edit(Question $question)
     {
+        $this->authorize('update', $question);
+
         return view("questions.edit", compact('question'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Question  $question
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Question            $question
+     *
      * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize('update', $question);
         $question->update($request->only('title', 'body'));
 
         return redirect()->route('questions.index')->with('success', "Your question has been updated.");
@@ -93,6 +105,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize('delete', $question);
         $question->delete();
 
         return redirect()->route('questions.index')->with('success', "Your question has been deleted.");
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -37,19 +37,26 @@
                                             <a href="{{ $question->url }}">{{ $question->title }}</a>
                                         </h3>
                                         <div class="ml-auto">
-                                            <a href="{{ route('questions.edit', $question->id) }}"
-                                               class="btn btn-sm btn-outline-info">Edit
-                                            </a>
+                                            @can('update', $question)
+                                                <a href="{{ route('questions.edit', $question->id) }}"
+                                                   class="btn btn-sm btn-outline-info">Edit
+                                                </a>
+                                            @endcan
                                         </div>
-                                        <form class="form-delete" action="{{ route('questions.destroy', $question->id) }}"
-                                              method="post">
-                                            @method('DELETE')
-                                            @csrf
-                                            <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')"
-                                            >
-                                                Delete
-                                            </button>
-                                        </form>
+                                        @can('delete', $question)
+                                            <form class="form-delete"
+                                                  action="{{ route('questions.destroy', $question->id) }}"
+                                                  method="post">
+                                                @method('DELETE')
+                                                @csrf
+                                                <button type="submit"
+                                                        class="btn btn-sm btn-outline-danger"
+                                                        onclick="return confirm('Are you sure?')"
+                                                >
+                                                    Delete
+                                                </button>
+                                            </form>
+                                        @endcan
                                     </div>
                                     <p class="lead">
                                         Asked by
